1e343b7b4d9fd9ee46129985b1d24ce05a93e4f1
ReniMehare <renimhre21@gmail.com>
2023-04-04 12:10:32 +0700

    pertemuan05

---
+++ pertemuan05/src/Card.jsx
@@ -0,0 +1,68 @@
+function Card() {
+    return (
+        <>
+            <div class="flex justify-center items-center h-screen w-full bg-gradient-to-r from-blue-500 to-blue-600">
+                <div class="w-1/2 bg-white rounded-lg shadow-2xl p-8 m-4">
+                    <h1 class="block w-full tect-center tect-gray-800 text-2xl font-bold mb-6">
+                        Registrasi
+                    </h1>
+                    <form action="/" method="post">
+                        <div class="flex flex-col mb-4">
+                        <label
+                                class="mb-2 font-bold text-lg text-gray-900"
+                                for="nama"
+                            >
+                                Nama
+                            </label>
+                            <input
+                                class="border py-2 px-3 text-grey-800"
+                                type="text"
+                                name="nama"
+                                id="nama"
+                            />
+                            <label
+                                class="mb-2 font-bold text-lg text-gray-900"
+                                for="NIK"
+                            >
+                                NIK 
+                            </label>
+                            <input
+                                 class="border py-2 px-3 text-grey-800"
+                                 type="text"
+                                 name="NIK"
+                                 id="nama"
+                            />
+                            <label
+                                class="mb-2 font-bold text-Ig text-gray-900"
+                                for="Alamat"
+                            >
+                                Alamat 
+                            </label>
+                            <input
+                             class="border py-2 px-3 text-grey-800"
+                             type="text"
+                             name="Alamat"
+                             id="nama"
+
+                             />                                                 
+                        </div>
+                        <button
+                        class="block bg-blue-400 hover:bg-blue-600 text-white uppercase text-lg mx-auto p-4 rounded"
+                        type="submit"
+                        >
+                            Buat Akun
+                        </button>
+                    </form>
+                    <a
+                        class="block w-full text-center no-underline mt-4 text-sm text-gray-700 hover:text-gry-900"
+                        href="/"
+                    >
+                        Telah Memiliki Akun?
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Card;
